b10812faf168da4b22bfb9a9aacf98e76ccec284
Slaby, Joel Benjamin <joelslaby@gatech.edu>
2023-03-21 14:57:38 -0400

    kcube motors fully working with labExt

--- LabExT/Movement/Stages/ThorlabsKCube.py
+++ LabExT/Movement/Stages/ThorlabsKCube.py
@@ -6,11 +6,16 @@ This program is free software and comes with ABSOLUTELY NO WARRANTY; for details
 """
 import sys
 import json
+import time
+import numpy as np
+
+from enum import Enum
 
 import py_thorlabs_ctrl.kinesis as KCUBE
-# py_thorlabs_ctrl.kinesis.init('C:\Program Files\Thorlabs\Kinesis')
+from System import Decimal
 
-from LabExT.Movement.Stage import Stage
+from LabExT.Movement.config import Axis
+from LabExT.Movement.Stage import Stage, StageError, assert_stage_connected, assert_driver_loaded
 from LabExT.Utils import get_configuration_file_path, try_to_lift_window
 from LabExT.View.Controls.DriverPathDialog import DriverPathDialog
 
@@ -31,6 +36,11 @@ finally:
     if sys_path_changed:
         del sys.path[0]
 
+class MovementType(Enum):
+    """Enumerate different movement modes."""
+    RELATIVE = 0,
+    ABSOLUTE = 1
+
 class ThorlabsKCube(Stage):
     """
     Simple Stage implementation for testing purposes.
@@ -44,10 +54,6 @@ class ThorlabsKCube(Stage):
     driver_path_dialog = None
     driver_specifiable = True
     description = "Thorlabs KCube Stages"
-
-    @classmethod
-    def find_stage_addresses(cls):
-        return [27258584, 27258581]
 
     @classmethod
     def load_driver(cls, parent):
@@ -68,15 +74,107 @@ class ThorlabsKCube(Stage):
         
         return cls.driver_path_dialog.path_has_changed
 
+    @classmethod
+    @assert_driver_loaded
+    def find_stage_addresses(cls): #TODO
+        return ["KCUBE"]
+    
+    class _Channel:
+        def __init__(self, serial_number, name='Channel') -> None:
+            self.name = name
+            self._sn = serial_number
+            self._status = None
+            self._movement_mode = MovementType.RELATIVE
+            self._position = None
+            self._sensor = None
+            self._speed = 0
+            self._acceleration = 0
+
+            self._stage = KCubeDCServo(self._sn)
+            self._stage.create()
+            self._stage.enable()
+        
+        @property
+        def status(self) -> int:
+            """Returns current channel status specified by SA_GetStatus_S"""
+            self._status = self._stage.get_device()
+
+            return self._status
+        
+        @property
+        def position(self):
+            """Returns current position of channel in micrometers specified by SA_GetPosition_S"""
+            self._position = self._stage.get_position() * 1e3
+
+            return self._position
+        
+        @property
+        def speed(self) -> float: #TODO
+            """Returns speed setting of channel in micrometers/seconds specified by SA_GetClosedLoopMoveSpeed_S"""
+            self._speed = Decimal.ToDouble(self._stage.get_device().GetVelocityParams().MaxVelocity) * 1e3
+
+            return self._speed
+        
+        @speed.setter
+        def speed(self, umps: float) -> None:
+            self._speed = self._stage.set_velocity(max_velocity=None, acceleration=None)
+
+        @property
+        def acceleration(self) -> float:
+            """Returns acceleration of channel in micrometers/seconds^2 specified by SA_GetClosedLoopMoveAcceleration_S"""
+            self._acceleration = self._stage.get_acceleration() * 1e3
+
+            return self._acceleration
+
+        @acceleration.setter
+        def acceleration(self, umps2: float, max_velocity=None) -> None:
+            """Sets acceleration of channel in micrometers/seconds^2 by calling SA_SetClosedLoopMoveAcceleration_S
+
+            Parameters
+            ----------
+            umps2 : float
+                Acceleration measured in um/s^2
+            """
+            accel = None if umps2 < 1e-8 else umps2/1e3
+            self._acceleration = self._stage.set_velocity(max_velocity=max_velocity, acceleration=accel)
+
+        # Channel control
+        def stop(self) -> None:
+            """Stops all movement of this channel"""
+            self._stage.disable()
+
+        def move(
+                self,
+                diff: float,
+                mode: MovementType) -> None:
+            """Moves the channel with the specified movement type by the value diff
+
+            Parameters
+            ----------
+            diff : float
+                Channel movement measured in micrometers.
+            mode : MovementType
+                Channel movement type
+            """
+            self.movement_mode = mode
+            if self.movement_mode == MovementType.RELATIVE:
+                print('rel: ', diff)
+                self._stage.move_relative(diff / 1e3)
+            elif self.movement_mode == MovementType.ABSOLUTE:
+                print('abs: ', diff)
+                self._stage.move_absolute(diff / 1e3)
+
+
     def __init__(self, address):
         super().__init__(address)
-
-        self._speed_xy = None
+        self.channels = {}
+        self.handle = None
         self._speed_z = None
-        self._acceleration_xy = None
+        self._speed_xy = 0
+        
 
     def __str__(self) -> str:
-        return "Dummy Stage at {}".format(self.address_string)
+        return "KCUBE Stage at {}".format(self.address_string)
 
     @property
     def address_string(self) -> str:
@@ -87,47 +185,132 @@ class ThorlabsKCube(Stage):
         return self.address_string
 
     def connect(self) -> bool:
-        self.connected = True
-        return True
+        if self.connected:
+            self._logger.debug('Stage is already connected.')
+            return True
+        
+        axes = [Axis.X, Axis.Y]
+        sns = [27258584, 27258581, None]
+        
+        for sn, axis in zip(sns, axes):
+            self.channels[axis] = self._Channel(serial_number=sn)
 
+            try:
+                self.connected = True
+
+                self._logger.info(
+                    'PiezoStage at {} initialised successfully.'.format(
+                        self.address))
+
+            except Exception as e:
+                self.connected = False
+                self.handle = None
+                self.channels = {}
+
+                raise e
+            
+        return self.connected
+
+    @assert_driver_loaded
+    # @assert_stage_connected
     def disconnect(self) -> bool:
+        for ch in self.channels:
+            ch._stage.disconnect()
         self.connected = False
-        return True
 
+    @assert_driver_loaded
+    # @assert_stage_connected
     def set_speed_xy(self, umps: float):
+        self.channels[Axis.X].speed = umps
+        self.channels[Axis.Y].speed = umps
         self._speed_xy = umps
 
+    @assert_driver_loaded
+    # @assert_stage_connected
     def set_speed_z(self, umps: float):
         self._speed_z = umps
 
+    @assert_driver_loaded
+    # @assert_stage_connected
     def get_speed_xy(self) -> float:
-        return self._speed_xy
+        x_speed = self.channels[Axis.X].speed
+        y_speed = self.channels[Axis.Y].speed
+        if (x_speed != y_speed):
+            self._logger.info(
+                "Speed settings of x and y channel are not equal.")
+            
+        return x_speed
 
+    @assert_driver_loaded
+    # @assert_stage_connected
     def get_speed_z(self) -> float:
         return self._speed_z
 
+    @assert_driver_loaded
+    # @assert_stage_connected
     def set_acceleration_xy(self, umps2):
-        self._acceleration_xy = umps2
+        self.channels[Axis.X].acceleration = umps2
+        self.channels[Axis.Y].acceleration = umps2
 
+    @assert_driver_loaded
+    # @assert_stage_connected
     def get_acceleration_xy(self) -> float:
-        return self._acceleration_xy
+        x_acceleration = self.channels[Axis.X].acceleration
+        y_acceleration = self.channels[Axis.Y].acceleration
 
-    def get_status(self) -> tuple:
+        if (x_acceleration != y_acceleration):
+            self._logger.info(
+                'Acceleration settings of x and y channel are not equal.')
+
+        return x_acceleration
+
+    @assert_driver_loaded
+    # @assert_stage_connected
+    def get_status(self) -> tuple: #TODO
         return ('STOP', 'STOP', 'STOP')
 
-    @property
-    def is_stopped(self) -> bool:
-        return all(s == 'STOP' for s in self.get_status())
+    def is_stopped(self, channel, stop_pos_um) -> bool: #TODO
+        tol = 1e-1 #tolerance in microns
+        print('Stopped check', channel.position, stop_pos_um)
+        return (np.abs(channel.position - stop_pos_um) <= tol) 
 
+    @assert_driver_loaded
+    # @assert_stage_connected
     def get_position(self) -> list:
-        return [0, 0, 0]
+        return [
+            self.channels[Axis.X].position,
+            self.channels[Axis.Y].position,
+            0
+        ]
 
+    @assert_driver_loaded
+    # @assert_stage_connected
     def move_relative(
             self,
             x: float = 0,
             y: float = 0,
             z: float = 0,
             wait_for_stopping: bool = True) -> None:
+        
+        self._logger.debug(
+            'Want to relative move %s to x = %s um, y = %s um and z = %s um',
+            self.address,
+            x,
+            y,
+            z)
+        
+        print("MOVING RELATIVE", x, y)
+
+        stop_pos_um = self.channels[Axis.X].position + x
+        self.channels[Axis.X].move(diff=x, mode=MovementType.RELATIVE)
+        if wait_for_stopping:
+            self._wait_for_stopping(self.channels[Axis.X], stop_pos_um)
+
+        stop_pos_um = self.channels[Axis.Y].position + y
+        self.channels[Axis.Y].move(diff=y, mode=MovementType.RELATIVE)
+        if wait_for_stopping:
+            self._wait_for_stopping(self.channels[Axis.Y], stop_pos_um)
+
         pass
 
     def move_absolute(
@@ -136,4 +319,33 @@ class ThorlabsKCube(Stage):
             y: float = None,
             z: float = None,
             wait_for_stopping: bool = True) -> None:
-        pass
+        
+        self._logger.debug(
+            'Want to absolute move %s to x = %s um, y = %s um and z = %s um',
+            self.address,
+            x,
+            y,
+            z)
+
+        if x is not None:
+            stop_pos_um = x
+            self.channels[Axis.X].move(diff=x, mode=MovementType.ABSOLUTE)
+            if wait_for_stopping:
+                self._wait_for_stopping(self.channels[Axis.X], stop_pos_um)
+        if y is not None:
+            stop_pos_um = y
+            self.channels[Axis.Y].move(diff=y, mode=MovementType.ABSOLUTE)
+            if wait_for_stopping:
+                self._wait_for_stopping(self.channels[Axis.Y], stop_pos_um)
+
+
+    def _wait_for_stopping(self, channel, stop_pos: float, delay=0.05):
+        """
+        Blocks until all channels have 'SA_STOPPED_STATUS' status.
+        """
+        print('wait for stopping')
+        while True:
+            time.sleep(delay)
+
+            if self.is_stopped(channel, stop_pos):
+                break
