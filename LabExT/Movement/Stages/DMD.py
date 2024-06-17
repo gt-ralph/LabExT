7354af4eb531b745c5202c760e49ec600a5e6540
Mihir-DG <dasguptamihir10@gmail.com>
2024-06-17 14:51:56 -0400

    DMD init

---
+++ LabExT/Movement/Stages/DMD.py
@@ -0,0 +1,125 @@
+import pyvisa as visa
+from LabExT.Movement.config import Axis
+from LabExT.Movement.Stage import Stage, StageError, assert_stage_connected, assert_driver_loaded
+from LabExT.Utils import get_configuration_file_path, try_to_lift_window
+from LabExT.View.Controls.DriverPathDialog import DriverPathDialog
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+LabExT  Copyright (C) 2022  ETH Zurich and Polariton Technologies AG
+This program is free software and comes with ABSOLUTELY NO WARRANTY; for details see LICENSE file.
+"""
+
+from LabExT.Movement.Stage import Stage
+
+"""
+Addresses:
+1. motors - 10 - newport esp300
+2. multimeter - 21 - agilent 34401A lab multimeter (hp)
+3. attenuator - 27 - exfoFVA-3100
+"""
+class DMD(Stage):
+    """
+    Simple Stage implementation for testing purposes.
+    """
+
+    #
+    #   Class description and properties
+    #
+
+    driver_loaded = True
+    driver_specifiable = False
+    description = "Dummy Stage Vendor"
+
+    @classmethod
+    def find_stage_addresses(cls):
+        return [
+            'tcp:192.168.0.42:1234',
+            'tcp:192.168.0.123:7894'
+        ]
+
+    @classmethod
+    def load_driver(cls):
+        pass
+
+    def __init__(self, address):
+        super().__init__(address)
+
+        """
+        self._speed_xy = None
+        self._speed_z = None
+        self._acceleration_xy = None"""
+
+    def __str__(self) -> str:
+        return "Dummy Stage at {}".format(self.address_string)
+
+    @property
+    def address_string(self) -> str:
+        return self.address
+
+    @property
+    def identifier(self) -> str:
+        return self.address_string
+
+    def connect(self) -> bool:
+        self.connected = True
+        return True
+
+    def disconnect(self) -> bool:
+        self.connected = False
+        return True
+
+    def set_speed_xy(self, umps: float):
+        self._speed_xy = umps
+
+    def set_speed_z(self, umps: float):
+        self._speed_z = umps
+
+    def get_speed_xy(self) -> float:
+        return self._speed_xy
+
+    def get_speed_z(self) -> float:
+        return self._speed_z
+
+    def set_acceleration_xy(self, umps2):
+        self._acceleration_xy = umps2
+
+    def get_acceleration_xy(self) -> float:
+        return self._acceleration_xy
+
+    def get_status(self) -> tuple:
+        return ('STOP', 'STOP', 'STOP')
+
+    @property
+    def is_stopped(self) -> bool:
+        return all(s == 'STOP' for s in self.get_status())
+
+    def get_position(self) -> list:
+        return [0, 0, 0]
+
+    def move_absolute(ax,pos, newport):
+	newport.write(f'{ax}PA{pos}')
+	newport.query(f'{ax}MD?')
+	resp = newport.read() # information on response from MD---> successful motion or not
+	while resp[0] == '0':
+		newport.query(f'{ax}MD?')
+		resp = newport.read()
+
+    
+    """
+    def move_relative(
+            self,
+            x: float = 0,
+            y: float = 0,
+            z: float = 0,
+            wait_for_stopping: bool = True) -> None:
+        pass
+
+    def move_absolute(
+            self,
+            x: float = None,
+            y: float = None,
+            z: float = None,
+            wait_for_stopping: bool = True) -> None:
+        pass"""
