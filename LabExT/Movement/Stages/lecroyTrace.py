530767f03138569d33a1ee2de32471c8560c311a
Mihir Dasgupta <dasguptamihir10@gmail.com>
2024-07-05 12:33:21 -0400

    Trace scripts added; not integrated w/LabExT; Dummy chip added

---
+++ LabExT/Movement/Stages/lecroyTrace.py
@@ -0,0 +1,99 @@
+import pyvisa
+import os
+from chardet import detect
+import sys
+from matplotlib import pyplot as plt
+import struct
+
+rm = pyvisa.ResourceManager()
+scope1 = rm.open_resource('TCPIP0::ts404-17::inst0::INSTR')
+scope1.write(f'COMM_FORMAT DEF9,BYTE,BIN')
+
+format = scope1.query(f'COMM_FORMAT?').strip()
+#print(format)
+
+
+def inspect():
+    traceStatus = scope1.query(f'C4:TRACE?')
+    data = scope1.query(f'C4:INSPECT? "SIMPLE"').strip()
+
+    offset = scope1.query(f'C4:OFFSET?').strip()
+    offset = float(offset.split()[1])
+
+    gain = scope1.query(f'C4:VDIV?').strip()
+    gain = float(gain.split()[1])
+    gain_mV = gain * 1000
+        
+    data = data.split()
+    final_data = []
+    for dp in data:
+        dp = dp.strip()
+        try:
+            final_data.append(gain_mV*float(dp)-offset)
+        except:
+            continue        
+    #plt.plot(final_data)
+    #plt.show()
+    return final_data
+
+def trace():
+    offset = scope1.query(f'C4:OFFSET?').strip()
+    offset = float(offset.split()[1])
+
+    gain = scope1.query(f'C4:VDIV?').strip()
+    gain = float(gain.split()[1])
+    gain_mV = gain * 1000
+    
+    #data = scope1.query_binary_values(f'C4:WAVEFORM?',is_big_endian = True)#,datatype='d')
+    scope1.write(f'C4:WAVEFORM?')
+    data = scope1.read_raw()
+    """#plt.plot(data)
+    filtered = list(filter(less_than_threshold,data))
+    #plt.hist(data,500)
+    plt.hist(filtered,100)
+    plt.show()"""
+    
+    print(len(data))
+    
+    data = struct.unpack('>' + 'h' *(len(data)//2), data)
+    decoded_values = [gain*point - offset for point in data]
+    #plt.plot(decoded_values[184:])
+    #plt.show()
+    return decoded_values[184:]
+
+
+traceOut = trace()
+inspectOut = inspect()
+
+plt.plot(inspectOut)
+plt.plot(traceOut)
+plt.show()
+
+
+
+
+"""
+
+THIS DOESNT WORK
+#print(scope1.query('*IDN?').strip())
+scope1.write(f'C4:WAVEFORM?')
+raw_data = scope1.read_raw()
+raw_data.decode()
+#raw_data = str(raw_data,'utf-8')
+#raw_data.encode('utf-8','ignore')
+detected_encoding = detect(raw_data)
+print(detected_encoding)
+
+
+# DECODE
+header_length = 2 + int(raw_data[1])
+print(type(raw_data))
+data_length = len(range(raw_data[2:header_length]))
+print(data_length)
+#waveform_data = raw_data[header_length:header_length+data_length]
+
+#raw_data = raw_data.decode('utf-8')
+
+
+scope1.close()
+"""
