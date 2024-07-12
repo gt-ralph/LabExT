79a72de9b0e118265ecc5b51eddf111d595e8bb6
Mihir Dasgupta <dasguptamihir10@gmail.com>
2024-07-12 11:13:41 -0400

    moving files around

--- LabExT/Instruments/MultimeterAgilent34401A.py
+++ LabExT/Instruments/MultimeterAgilent34401A.py
@@ -1,16 +1,20 @@
-from LabExT.Instruments.InstrumentAPI import Instrument, InstrumentException
+from LabExT.Instruments.InstrumentAPI import Instrument
 
 class MultimeterAgilent34401A(Instrument):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwards)
+        super().__init__(*args, **kwargs)
 
     def open(self):
         super().open()
-
+        self._inst.query("*IDN?")
+        """
         self._inst.read_termination = '\r\n'
 
         authentication = self._inst.query('open "anonymous"')
         ready = self._inst.query(" ")
 
         if authentication != 'AUTHENTICATE CRAM-MD5.' or ready != 'ready':
-            raise InstrumentException('Authentication failed')
+            raise InstrumentException('Authentication failed')"""
+    
+    def get_voltage_reading(self):
+        pass
